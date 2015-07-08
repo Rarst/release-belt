f406beb784e45798c1c3e06a9e416bd52a8cac7e
Andrey Savchenko <contact@rarst.net>
2015-07-09 00:21:55 +0300

    Added unit test for parsing file names

---
+++ tests/ReleaseTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Rarst\ReleaseBelt;
+
+class ReleaseTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider filenameProvider
+     *
+     * @param string $filename
+     * @param string $package
+     * @param string $version
+     */
+    public function testParseFilename($filename, $package, $version)
+    {
+        $splFileInfoMock = $this->getMockBuilder('Symfony\Component\Finder\SplFileInfo')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $splFileInfoMock->expects($this->once())
+            ->method('getRelativePath')
+            ->willReturn('type/vendor');
+
+        $splFileInfoMock->expects($this->once())
+            ->method('getFilename')
+            ->willReturn($filename);
+
+        $release = new Release($splFileInfoMock);
+
+        $this->assertEquals($package, $release->package);
+        $this->assertEquals($version, $release->version);
+    }
+
+    public function filenameProvider()
+    {
+        return [
+            [ 'backupbuddy-4.1.2.2.zip', 'backupbuddy', '4.1.2.2' ],
+//            [ 'contact-form-7.4.1.zip', 'contact-form', '4.1' ],
+            [ 'facetwp-1.8.6.zip', 'facetwp', '1.8.6' ],
+            [ 'ga-ecommerce-3.0.2.zip', 'ga-ecommerce', '3.0.2' ],
+            [ 'google-analytics-premium-1.1.8.zip', 'google-analytics-premium', '1.1.8' ],
+            [ 'gravityforms_1.8.22.zip', 'gravityforms', '1.8.22' ],
+            [ 'gravityformsmailchimp_3.2.zip', 'gravityformsmailchimp', '3.2' ],
+            [ 'mailchimp-for-wp-pro-2.5.5.zip', 'mailchimp-for-wp-pro', '2.5.5' ],
+            [ 'nextgen-facebook.7.7.5.1.zip', 'nextgen-facebook', '7.7.5.1' ],
+            [ 'ninja-forms-mailchimp-v1.3.2.zip', 'ninja-forms-mailchimp', '1.3.2' ],
+            [ 'p3-profiler.1.5.3.6.zip', 'p3-profiler', '1.5.3.6' ],
+            [ 'searchwp-2.4.9.zip', 'searchwp', '2.4.9' ],
+            [ 'searchwp-term-highlight-1.8.7.zip', 'searchwp-term-highlight', '1.8.7' ],
+            [ 'sitepress-multilingual-cms.3.1.8.4.zip', 'sitepress-multilingual-cms', '3.1.8.4' ],
+            [ 'wp-retina-2x.2.4.0.zip', 'wp-retina-2x', '2.4.0' ],
+            [ 'wp-lightbox-2.zip', 'wp-lightbox', '2' ],
+            [ 'wpml-translation-management.1.9.9.zip', 'wpml-translation-management', '1.9.9' ],
+        ];
+    }
+}
