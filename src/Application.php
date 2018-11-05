f682adf017d6b934602342a126017ed8e3d8c02e
Andrey Savchenko <contact@rarst.net>
2018-11-05 14:25:55 +0200

    Changed main application class for Slim.

--- src/Application.php
+++ src/Application.php
@@ -2,62 +2,73 @@
 
 namespace Rarst\ReleaseBelt;
 
-use Mustache\Silex\Application\MustacheTrait;
-use Mustache\Silex\Provider\MustacheServiceProvider;
 use Rarst\ReleaseBelt\Provider\AuthenticationProvider;
 use Rarst\ReleaseBelt\Provider\DownloadsLogProvider;
 use Rarst\ReleaseBelt\Provider\FractalProvider;
 use Rarst\ReleaseBelt\Provider\ModelProvider;
-use Silex\Provider\MonologServiceProvider;
-use Silex\Application\MonologTrait;
-use Silex\Provider\SecurityServiceProvider;
+use Slim\App;
+use Slim\Container;
 use Symfony\Component\Finder\Finder;
 
-class Application extends \Silex\Application
+class Application extends App
 {
-    use MustacheTrait, MonologTrait, DownloadsLogTrait;
+    use DownloadsLogTrait;
 
+    /**
+     * Main application constructor.
+     *
+     * Belt is hardwired to use Pimple since Silex times, so overriding Slim’s container object is not supported.
+     *
+     * @param array $values
+     */
     public function __construct(array $values = [])
     {
-        parent::__construct();
+        $settings = [];
 
-        $app = $this;
+        if (isset($values['settings'])) { // These are handled/used early by Slim’s internal registrations.
+            $settings = $values['settings'];
+            unset($values['settings']);
+        }
+
+        parent::__construct(['settings' => $settings]);
+        /** @var Container $container */
+        $container = $this->getContainer();
 
         /** @deprecated 0.3:1.0 Deprecated in favor of `users`. */
-        $app['http.users']  = [];
-        $app['users']       = [];
-        $app['release.dir'] = __DIR__.'/../releases';
-        $app['finder']      = function () {
+        $container['http.users']  = [];
+        $container['users']       = [];
+        $container['release.dir'] = __DIR__.'/../releases';
+        $container['finder']      = function () use ($container) {
             $finder = new Finder();
-            $finder->files()->in($this['release.dir']);
+            $finder->files()->in($container['release.dir']);
 
             return $finder;
         };
-        $app['parser']      = function () use ($app) {
-            return new ReleaseParser($app['finder']);
+        $container['parser']      = function () use ($container) {
+            return new ReleaseParser($container['finder']);
         };
 
-        $app->register(new ModelProvider());
-        $app->register(new FractalProvider());
-        $app->register(new DownloadsLogProvider());
-        $app->register(new AuthenticationProvider());
+        $container->register(new ModelProvider());
+        $container->register(new FractalProvider());
+        $container->register(new DownloadsLogProvider());
+//        $app->register(new AuthenticationProvider());
 
-        $app->register(new MustacheServiceProvider, [
-            'mustache.path' => __DIR__.'/mustache',
-        ]);
-        $app->register(new MonologServiceProvider());
-        $app->register(new SecurityServiceProvider());
+//        $app->register(new MustacheServiceProvider, [
+//            'mustache.path' => __DIR__.'/mustache',
+//        ]);
+//        $app->register(new MonologServiceProvider());
+//        $app->register(new SecurityServiceProvider());
 
-        $app->get('/', 'Rarst\\ReleaseBelt\\Controller::getHtml');
+        $this->get('/', 'Rarst\\ReleaseBelt\\Controller:getHtml');
 
-        $app->get('/packages.json', 'Rarst\\ReleaseBelt\\Controller::getJson')
-            ->bind('json');
+        $this->get('/packages.json', 'Rarst\\ReleaseBelt\\Controller:getJson')
+            ->setName('json');
 
-        $app->get('/{vendor}/{file}', 'Rarst\\ReleaseBelt\\Controller::getFile')
-            ->bind('file');
+        $this->get('/{vendor}/{file}', 'Rarst\\ReleaseBelt\\Controller:getFile')
+            ->setName('file');
 
         foreach ($values as $key => $value) {
-            $this[$key] = $value;
+            $container[$key] = $value;
         }
     }
 }
