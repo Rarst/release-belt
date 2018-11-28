6f411581307ace77292e554b961102b4f5b73189
Andrey Savchenko <contact@rarst.net>
2018-11-28 18:17:58 +0200

    Moved default service definitions to a separate provider.

--- src/Application.php
+++ src/Application.php
@@ -3,20 +3,16 @@ declare(strict_types=1);
 
 namespace Rarst\ReleaseBelt;
 
-use Mustache_Loader_FilesystemLoader;
 use Psr\Http\Message\ServerRequestInterface;
 use Rarst\ReleaseBelt\Provider\AuthenticationProvider;
 use Rarst\ReleaseBelt\Provider\ControllerProvider;
+use Rarst\ReleaseBelt\Provider\DefaultsProvider;
 use Rarst\ReleaseBelt\Provider\DownloadsLogProvider;
 use Rarst\ReleaseBelt\Provider\FractalProvider;
 use Rarst\ReleaseBelt\Provider\ModelProvider;
 use RKA\Middleware\IpAddress;
 use Slim\App;
 use Slim\Container;
-use Slim\Http\Environment;
-use Slim\Http\Request;
-use Slim\Views\Mustache;
-use Symfony\Component\Finder\Finder;
 
 /**
  * Main application class and entry point.
@@ -46,46 +42,15 @@ class Application extends App
         /** @var Container $container */
         $container = $this->getContainer();
 
-        $container['debug'] = false;
-        /** @deprecated 0.3:1.0 Deprecated in favor of `users`. */
-        $container['http.users']  = [];
-        $container['users']       = [];
-        $container['release.dir'] = __DIR__.'/../releases';
-        $container['finder']      = function () use ($container) {
-            $finder = new Finder();
-            $finder->files()->in($container['release.dir']);
-
-            return $finder;
-        };
-        $container['parser']      = function () use ($container) {
-            return new ReleaseParser($container['finder']);
-        };
-        $container['username'] = function () use ($container) {
-            /** @var Environment $environment */
-            $environment = $container['environment'];
-            return $environment->get('PHP_AUTH_USER', '');
-        };
-        $container['url_generator'] = function () use ($container) {
-            /** @var Request $request */
-            $request = $container['request'];
-            return new UrlGenerator($container['router'], $request->getUri());
-        };
-        $container['view'] = function () {
-            $view = new Mustache([
-                'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/mustache'),
-            ]);
-
-            return $view;
-        };
-
-        $container->extend('request', function (ServerRequestInterface $request) use ($container) {
-            return $request->withAttribute('username', $container['username']);
-        });
-
+        $container->register(new DefaultsProvider());
         $container->register(new ModelProvider());
         $container->register(new ControllerProvider());
         $container->register(new FractalProvider());
         $container->register(new DownloadsLogProvider());
+
+        $container->extend('request', function (ServerRequestInterface $request) use ($container) {
+            return $request->withAttribute('username', $container['username']);
+        });
 
         $this->get('/', 'controller.index')->setName('index');
         $this->get('/packages.json', 'controller.json')->setName('json');
---
+++ src/Provider/DefaultsProvider.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Rarst\ReleaseBelt\Provider;
+
+use Mustache_Loader_FilesystemLoader;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Rarst\ReleaseBelt\ReleaseParser;
+use Rarst\ReleaseBelt\UrlGenerator;
+use Slim\Http\Environment;
+use Slim\Http\Request;
+use Slim\Views\Mustache;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Provides default services.
+ */
+class DefaultsProvider implements ServiceProviderInterface
+{
+    /**
+     * Performs service registrations.
+     */
+    public function register(Container $container): void
+    {
+        $container['debug'] = false;
+        /** @deprecated 0.3:1.0 Deprecated in favor of `users`. */
+        $container['http.users']    = [];
+        $container['users']         = [];
+        $container['release.dir']   = __DIR__.'/../../releases';
+        $container['finder']        = function () use ($container) {
+            $finder = new Finder();
+            $finder->files()->in($container['release.dir']);
+
+            return $finder;
+        };
+        $container['parser']        = function () use ($container) {
+            return new ReleaseParser($container['finder']);
+        };
+        $container['username']      = function () use ($container) {
+            /** @var Environment $environment */
+            $environment = $container['environment'];
+
+            return $environment->get('PHP_AUTH_USER', '');
+        };
+        $container['url_generator'] = function () use ($container) {
+            /** @var Request $request */
+            $request = $container['request'];
+
+            return new UrlGenerator($container['router'], $request->getUri());
+        };
+        $container['view']          = function () {
+            $view = new Mustache([
+                'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/../mustache'),
+            ]);
+
+            return $view;
+        };
+    }
+}
