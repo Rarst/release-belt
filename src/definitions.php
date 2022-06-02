fa3d2f10a1987dc1656fe41897c241093e59814b
Andrey Savchenko <contact@rarst.net>
2022-06-02 11:14:04 +0300

    Compacted and sorted definitions

--- src/definitions.php
+++ src/definitions.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use League\Fractal\Manager as Fractal;
@@ -15,53 +16,40 @@ use Slim\App;
 use Slim\Interfaces\RouteParserInterface;
 use Slim\Views\Mustache;
 use Symfony\Component\Finder\Finder;
+
 use function DI\autowire;
 use function DI\create;
 use function DI\get;
 
 return [
-    Collection::class            => function (ReleaseParser $parser, ReleaseTransformer $transformer) {
-        return new Collection($parser->getReleases(), $transformer);
-    },
+    Collection::class            => create()->constructor(get('releases'), get(ReleaseTransformer::class)),
     'data'                       => function (Fractal $fractal, Collection $collection) {
         return $fractal->createData($collection)->toArray();
     },
     'debug'                      => false,
     Finder::class                => function (ContainerInterface $container) {
-        $finder = new Finder();
-        $finder->files()->in($container->get('release.dir'));
-
-        return $finder;
+        return (new Finder())->files()->in($container->get('release.dir'));
     },
     Fractal::class               => create()->method('setSerializer', get(PackageSerializer::class)),
     /** @deprecated 0.3:1.0 Deprecated in favor of `users`. */
     'http.users'                 => [],
-    JsonController::class        => create()->constructor(get('data'), get('debug')),
     IndexModel::class            => autowire()
         ->constructorParameter('packages', get('packages'))
         ->constructorParameter('username', get('username')),
-    Mustache::class              => function () {
-        return new Mustache([
-            'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/mustache'),
-        ]);
-    },
-    'packages'                   => function (ContainerInterface $container): array {
-        return $container->get('data')['packages'];
-    },
-    'release.dir'                => __DIR__.'/../releases',
-    ReleaseTransformer::class    => function (UrlGeneratorInterface $urlGenerator) {
-        return new ReleaseTransformer($urlGenerator, array_merge(
-            require __DIR__.'/../config/installerTypes.php',
-            require __DIR__.'/../config/installerTypesV2.php'
-        ));
-    },
-    RouteParserInterface::class  => function (App $app) {
-        return $app->getRouteCollector()->getRouteParser();
-    },
-    UrlGeneratorInterface::class => autowire(UrlGenerator::class)
-        ->constructorParameter('uri', get('request.uri')),
-    'username'                   => function (): string {
-        return (string)($_SERVER['PHP_AUTH_USER'] ?? '');
-    },
+    'installerTypes'             => fn() => array_merge(
+        require __DIR__ . '/../config/installerTypes.php',
+        require __DIR__ . '/../config/installerTypesV2.php'
+    ),
+    JsonController::class        => create()->constructor(get('data'), get('debug')),
+    Mustache::class              => fn() => new Mustache([
+        'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/mustache'),
+    ]),
+    'packages'                   => fn(ContainerInterface $container) => $container->get('data')['packages'],
+    'release.dir'                => __DIR__ . '/../releases',
+    'releases'                   => fn(ReleaseParser $parser) => $parser->getReleases(),
+    ReleaseTransformer::class    => autowire()->constructorParameter('installerTypes', get('installerTypes')),
+    RouteParserInterface::class  => fn(App $app) => $app->getRouteCollector()->getRouteParser(),
+    UrlGeneratorInterface::class => autowire(UrlGenerator::class)->constructorParameter('uri', get('request.uri')),
+    'username'                   => fn(): string => (string)($_SERVER['PHP_AUTH_USER'] ?? ''),
     'users'                      => [],
 ];
